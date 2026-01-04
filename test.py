74f398e9615c4e9d6fcdceb1aa75d1326e7e8056
Denis Mareev <98119367+DerasGertony@users.noreply.github.com>
2026-01-04 14:25:33 +0300

    tried async ollama

--- test.py
+++ test.py
@@ -1,26 +1,22 @@
 import coir
 from coir.data_loader import get_tasks
 from coir.evaluation import COIR
-import torch
-import numpy as np
-import logging
-from transformers import AutoTokenizer, AutoModel
-from typing import List, Dict
-from tqdm.auto import tqdm
+from coir.models import YourCustomDEModel
+import ollama
 
-
+model_name = "intfloat/e5-base-v2"
 
 # Load the model
-model = APIModel()
+model = YourCustomDEModel(model_name=model_name)
 
 # Get tasks
-#all task ["codetrans-dl", "stackoverflow-qa", "apps","codefeedback-mt", "codefeedback-st", "codetrans-contest", "synthetic-
-# text2sql", "cosqa", "codesearchnet", "codesearchnet-ccr"]
-tasks = coir.get_tasks(tasks=["codetrans-dl"])
+#all task ["codetrans-dl","stackoverflow-qa","apps","codefeedback-mt","codefeedback-st","codetrans-contest","synthetic-
+# text2sql","cosqa","codesearchnet","codesearchnet-ccr"]
+tasks = get_tasks(tasks=["codetrans-dl"])
 
 # Initialize evaluation
-evaluation = COIR(tasks=tasks, batch_size=128)
+evaluation = COIR(tasks=tasks,batch_size=128)
 
 # Run evaluation
-results = evaluation.run(model, output_folder=f"results/")
+results = evaluation.run(model, output_folder=f"results/{model_name}")
 print(results)

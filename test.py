453e4e1e9f50afac0098c120c398764059f76d0c
Denis Mareev <98119367+DerasGertony@users.noreply.github.com>
2026-01-18 13:47:29 +0300

    llm+lex+hybrid +saving sys

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ from coir.evaluation import COIR
 from coir.models import YourCustomDEModel
 import ollama
 
+
+poss_LLM= {"llama3.2", "gemma3", "mistral"}
+
+
+
 model_name = "intfloat/e5-base-v2"
+chos_LLMmodel = {"mistral"}
+useLLm = False
 
 # Load the model
 model = YourCustomDEModel(model_name=model_name)
@@ -13,10 +20,17 @@ model = YourCustomDEModel(model_name=model_name)
 #all task ["codetrans-dl","stackoverflow-qa","apps","codefeedback-mt","codefeedback-st","codetrans-contest","synthetic-
 # text2sql","cosqa","codesearchnet","codesearchnet-ccr"]
 tasks = get_tasks(tasks=["codetrans-dl"])
-
+##flag for requeueing
 # Initialize evaluation
-evaluation = COIR(tasks=tasks,batch_size=128)
+evaluation = COIR(tasks=tasks,batch_size=64, type ="hybrid")
 
-# Run evaluation
-results = evaluation.run(model, output_folder=f"results/{model_name}")
+    
+if useLLm and chos_LLMmodel:
+    for llm in chos_LLMmodel.intersection(poss_LLM):
+        results = evaluation.run(model, output_folder=f"results", useLLm=useLLm, llmname=llm)
+else:
+    # Run evaluation
+    results = evaluation.run(model, output_folder="results", useLLm=useLLm, llmname='')
+    
+
 print(results)

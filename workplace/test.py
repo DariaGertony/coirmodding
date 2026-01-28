d57bea9aa9a22e0485423b881c71779fff85205a
Vadim <109895300+mahmud-off@users.noreply.github.com>
2026-01-28 14:55:46 +0300

    code chiking/refactoring

--- workplace/test.py
+++ workplace/test.py
@@ -11,7 +11,7 @@ poss_LLM= {"llama3.2", "gemma3", "phi3"}
 
 
 
-model_name = "BAAI/bge-m3"
+model_name = "intfloat/e5-base-v2"
 chos_LLMmodel = {"phi3"}
 useLLm = False
 rerank = True
@@ -25,10 +25,10 @@ model = YourCustomDEModel(model_name=model_name)
 # Get tasks
 #all task ["codetrans-dl","stackoverflow-qa","apps","codefeedback-mt","codefeedback-st","codetrans-contest","synthetic-
 # text2sql","cosqa","codesearchnet","codesearchnet-ccr"]
-tasks = get_tasks(tasks=["stackoverflow-qa"])
+tasks = get_tasks(tasks=["codetrans-dl"])
 ##flag for requeueing
 # Initialize evaluation
-evaluation = COIR(tasks=tasks,batch_size=64, type ="semantic")
+evaluation = COIR(tasks=tasks,batch_size=64, type ="hybrid_bm25")
 
     
 if useLLm and chos_LLMmodel:

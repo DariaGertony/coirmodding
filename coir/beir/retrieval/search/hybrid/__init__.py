74217136e8c4732c616bb772c1b0d101bc165d50
Denis Mareev <98119367+DerasGertony@users.noreply.github.com>
2026-02-03 20:02:31 +0300

    Revert "Реструктуризация репозитория"
    This reverts commit 2d1662eb677f895c46d872093ef57b6c11c5fc23, reversing
    changes made to 184a26526868ac627679581543a1e4f7b701c547.

--- coir/beir/retrieval/search/hybrid/__init__.py
+++
@@ -1,5 +0,0 @@
-from .simple_hybrid_search import SimpleHybridSearch
-from .advanced_hybrid_search import AdvancedHybridSearch
-from .fusion_strategies import FusionStrategies
-
-__all__ = ['SimpleHybridSearch', 'AdvancedHybridSearch', 'FusionStrategies']
--- coir/beir/retrieval/search/lexical/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bm25_search import LexicalBM25Search
-from .jaccard_search import LexicalJaccardSearch
-
-__all__ = ['LexicalBM25Search', 'LexicalJaccardSearch']
